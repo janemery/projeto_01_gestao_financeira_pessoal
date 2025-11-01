59dfaf681cbbef895977dbacb483772299b87286
Jane Mery <janymeryc@gmail.com>
2025-11-01 08:15:43 -0300

    create de method receita

--- sistema_financeiro.py
+++ sistema_financeiro.py
@@ -1,1 +1,12 @@
-# sistema_financeiro.py
+def criar_receita(tipo, descricao, valor, data):
+    receita = {
+        'tipo': tipo,
+        'descricao': descricao,
+        'valor': valor,
+        'data': data
+    }
+    return receita
+
+# print minha receita
+minha_receita = criar_receita('receita', 'Salário', 5000.00, '2024-01-15')
+print(minha_receita)
